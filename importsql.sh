e1dc414731b654ae8b42bd75c88f6bc7299b4dcf
ivessss <841510983@qq.com>
2024-08-15 10:56:39 +0800

    1

--- importsql.sh
+++ importsql.sh
@@ -1,30 +1,3 @@
-#!/bin/bash
-
-# 数据库登录信息
-DB_USER="root"          # 替换为你的数据库用户名
-DB_PASSWORD="abcABC!@#123"  # 替换为你的数据库密码
-DB_HOST="124.222.26.224"             # 替换为你的数据库主机名
-
-# 数据文件的根目录
-SQL_DIR="/train_sql"  # 替换为你实际的路径
-#SQL_DIR="/dev_sql"
-# 遍历每个文件夹
-for db_dir in "$SQL_DIR"/*; do
-    if [ -d "$db_dir" ]; then
-        # 提取数据库名称（文件夹名称）
-        db_name=$(basename "$db_dir")
-        
-        echo "正在创建数据库：$db_name"
-        mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST -e "CREATE DATABASE IF NOT EXISTS $db_name;"
-        
-        # 遍历每个.sql文件
-        for sql_file in "$db_dir"/*.sql; do
-            if [ -f "$sql_file" ]; then
-                echo "正在导入 $sql_file 到数据库 $db_name"
-                mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST $db_name < "$sql_file"
-            fi
-        done
-    fi
-done
-
-echo "所有数据库和表已导入完成！"
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d84b3af6265669723dbe720e093c31814bf8d76c2da234405a8a483ebbf1064
+size 1028
